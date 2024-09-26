1e11f353dd35419e1ee802ced7ba55a4d2d79af6
Flot-dog <florence.o.ross@gmail.com>
2024-09-26 18:27:55 -0400

    Initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    printf("Spreadsheet Menu\n");
+    printf("________________\n");
+    printf("1. Display Spreadsheet\n");
+    printf("2. Display Histogram\n");
+    printf("3. Set Sort Column\n");
+    printf("4. Update Last Name\n");
+    printf("5. Update Exam Grade\n");
+    printf("6. Update Grade Mapping\n");
+    printf("7. Delete Student\n");
+    printf("8. Exit\n\n");
+    printf("Selection: \n");
+    return 0;
+}
